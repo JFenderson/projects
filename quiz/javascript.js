047c104beab3acd21b3e35699bf2466e1778c98d
JFenderson <fenderson.joseph@gmail.com>
2018-02-06 16:58:14 -0600

    finish quiz

--- quiz/javascript.js
+++ quiz/javascript.js
@@ -1,17 +1,66 @@
-const button = document.getElementById('makeFriends');
-const makeFriends = document.getElementById('friendsList');
-const friends = ['rick','ian','book','michael','darrius','magic','horacio','JJ','bennie','snipes']
 
+
+const obj = {
+    name: 'yaz',
+    email: 'yaz@yaz.com',
+    friends: [
+        {
+            name: 'jahan',
+            email: 'jahan@jahan.com'
+        },
+        {
+            name: 'rihanna',
+            email: 'rihanna@rihanna.com'
+        }
+    ]
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const friends = ['rick','ian','book','michael','darrius','magic','horacio','JJ','bennie','snipes']
+const sentence = 'hello world my name is john doe, goodbye forever'
 
 document.addEventListener('DOMContentLoaded',()=>{
     console.log('ready');
-})
-
+const button = document.getElementById('makeFriends');
+const friendsList = document.getElementById('friendsList');
 
 button.addEventListener('click',()=>{
-    for(i=0;i<friends.length;i++)
-    console.log(friends[i]);
-    document.createElement("li");
-    li.className('friendItem');
+    if(friendsList.chidlen.length === friends.length){
+        return;
+    }
+
+
+    
+    const li = document.createElement("li");
+    li.innerText - friends[friendsList.children.length];
+    friendsList.appendChild(li);
+    
     // makeFriends.appendChild('friendItem');
-    })
+    });
+
+
+});
+
+
+
+
+
